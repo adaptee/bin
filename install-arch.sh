5cf8fed7ec72ef3bad373f9c6d927d67784a40f0
Adaptee <adaptee@gmail.com>
2010-11-20 05:34:34 +0800

    improve naming style

--- install-arch.sh
+++ install-arch.sh
@@ -1396,7 +1396,7 @@ dbusmenu-qt-git
 #as dependency of kde-snapshot/kdepim-kaddressbook
 grantlee
 
-kdesupport-phonon 与 phonon-pulse 有冲突
-phonon-gstreamer-4.4.2-2 replaced by kdesupport-phonon
+diff2html
 
-phonoe-xine (deleted)
+jabberbot
+
